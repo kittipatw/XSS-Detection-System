ef2e9172b96a9578517b0d652f05c1f7155d450a
Kittipat Wattanasuwan <6322771153@g.siit.tu.ac.th>
2023-10-23 22:58:02 +0700

    Add link to dashboard on feedback.php

--- main/feedback.php
+++ main/feedback.php
@@ -38,7 +38,7 @@ if (!isset($_SESSION["user"])) {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item mx-2">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="/main/dashboard.php">Home</a>
           </li>
           <li class="nav-item mx-2">
             <a class="nav-link" href="/main/sale.php">Make Sale</a>
